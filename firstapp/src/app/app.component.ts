d39dcdd045fcbb33a2395fa391cf1835f56a0fe7
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-03-21 20:02:02 +0530

    uodate

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component'
+import { UserComponent } from './user/user.component';
 
 
 @Component({
   selector: 'app-ar',
   standalone: true,
-  imports: [HeaderComponent,],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
