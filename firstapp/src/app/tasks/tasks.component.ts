42dadde9e51878fa97563bb9e05a9a66586acb13
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-03-26 20:07:18 +0530

    update
    output , add task cmp and pass the user name to it

---
+++ firstapp/src/app/tasks/tasks.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+
+  get imagePath() {
+  return 'assets/users/' + this.avatar;
+}
+
+}
