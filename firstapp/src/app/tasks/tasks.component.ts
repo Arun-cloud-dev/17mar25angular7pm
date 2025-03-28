2c1749b6c3cf59a67b9cec7e3de4fe70a8861d88
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-03-28 08:09:55 +0530

    using signals

--- firstapp/src/app/tasks/tasks.component.ts
+++ firstapp/src/app/tasks/tasks.component.ts
@@ -7,11 +7,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
-  @Input({ required: true }) avatar!: string;
 
-  get imagePath() {
-  return 'assets/users/' + this.avatar;
-}
+
 
 }
--- firstapp/src/app/user/user.component.ts
+++ firstapp/src/app/user/user.component.ts
@@ -1,23 +1,29 @@
-import { Component, Input, Output , EventEmitter } from '@angular/core';
-
+import { Component, Input, input,computed ,} from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  
+
 })
 export class UserComponent {
-  @Input ({required :true }) id!: string;
-  @Input({required :true }) avatar!: string;
-  @Input({required :true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  onSelectUser() {
-    this.select.emit(this.id)
-  }
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+});
+
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
+
+  onSelectUser() {}
 }
