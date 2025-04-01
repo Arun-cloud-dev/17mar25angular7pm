6334a297eae649174a5ddff4f93f407667ad08bc
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-01 20:16:46 +0530

    update custom events

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -3,18 +3,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-ar',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    console.log('Selected user with id: ' + id); 
+    
   }
 }
--- firstapp/src/app/user/user.component.ts
+++ firstapp/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input,computed ,} from '@angular/core';
+import { Component, Input, input,computed, Output, EventEmitter , output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,19 +11,27 @@ import { Component, Input, input,computed ,} from '@angular/core';
 export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string; 
 
   avatar = input.required<string>();
   name = input.required<string>();
+
+  @Output() select = new EventEmitter(); 
+
+  // select = output<string>();
 
 
   imagePath = computed(() => {
     return 'assets/users/' + this.avatar();
 });
 
-
+  
   // get imagePath() {
   //   return 'assets/users/' + this.avatar;
   // }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+
+  }
 }
