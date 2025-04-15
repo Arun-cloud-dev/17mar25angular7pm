a465e18e104b320b62e698841e30bc52249e0bfe
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-04-15 19:54:46 +0530

    update

--- firstapp/src/app/user/user.component.ts
+++ firstapp/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, input,computed, Output, EventEmitter , output} from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 // interface User {
 //   id: string;
@@ -15,6 +16,7 @@ import { type User } from './user.model';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
   
 
 })
